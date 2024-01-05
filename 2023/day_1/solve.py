46341efd90a83ecd4c8feb3d81bad80391f0eb0e
ZezoHR <iabdulazizalharthi@gmail.com>
2024-01-05 18:12:09 +0300

    2023 day 1 part 2 solution

--- 2023/day_1/solve.py
+++ 2023/day_1/solve.py
@@ -16,14 +16,35 @@ def solve_1(path):
                 last_digit = calibration[i]
                 break
         total += int(first_digit + last_digit)
-        print(int(first_digit + last_digit))
     print(total)
 
 
 def solve_2(path):
     with open(path) as f:
         data = [line.strip() for line in f.readlines()]
-    ...
+    number = {
+        "one": '1',
+        "two": '2',
+        "three": '3',
+        "four": '4',
+        "five": '5',
+        "six": '6',
+        "seven": '7',
+        "eight": '8',
+        "nine": '9',
+    }
+    total = 0
+    for calibration in data:
+        digits = []
+        for i in range(len(calibration)):
+            if calibration[i].isdigit():
+                digits.append(calibration[i])
+            else:
+                for key in number.keys():
+                    if calibration[i:].startswith(key):
+                        digits.append(number[key])
+        total += int(digits[0] + digits[-1])
+    print(total)
 
 
 #################################################
